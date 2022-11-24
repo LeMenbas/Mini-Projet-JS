305f14241badc899b3a6d19d9bf8e498c6842fd5
LeMenbas <youssef.gh@live.fr>
2022-11-24 22:34:20 +0100

    first commit

---
+++ bgColor-Change/main.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('button')
+const colors = ["green", "red", "blue", "yellow"]
+const color = document.querySelector('.color')
+const main = document.querySelector('main')
+
+button.addEventListener('click', function() {
+    const numRandom = getRandomNumber();
+    console.log(numRandom)
+    main.style.backgroundColor = colors[numRandom];
+    color.textContent = colors[numRandom]
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length)
+};
