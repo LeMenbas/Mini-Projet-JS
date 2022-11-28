72b08ea3838421f54351bab555ba6873a3edc60d
LeMenbas <youssef.gh@live.fr>
2022-11-28 01:58:23 +0100

    rajout de contenue

--- Quiz-Model/index.js
+++ Quiz-Model/index.js
@@ -26,3 +26,32 @@ btn3.addEventListener("click",() => {
     response.style.visibility = "visible"
     response.style.border = "2px solid red"
 })
+
+
+// click event question 2
+
+const btn4 = document.querySelector("#btn-4");
+const btn5 = document.querySelector('#btn-5');
+const btn6 = document.getElementById("btn-6");
+const response2 = document.getElementById('reponse-2');
+const section2 = document.querySelector("#question2");
+const choix2 = document.getElementById("choise2");
+
+btn4.addEventListener("click",() => {
+    choix2.style.visibility = 'visible'
+    choix2.innerHTML = "Vous avez choisi : 'Marseille'"
+    response2.style.visibility = "visible"
+    response2.style.border = "2px solid red"
+})
+btn5.addEventListener("click",() => {
+    choix2.style.visibility= "visible"
+    choix2.innerHTML = "vous avez choisi : 'Lyon' "
+    response2.style.visibility = "visible"
+    response2.style.border = "2px solid red"
+})
+btn6.addEventListener("click",() => {
+    choix2.style.visibility= "visible"
+    choix2.innerHTML = "Super"
+    response2.style.visibility = "visible"
+    response2.style.border = "2px solid green"
+})
